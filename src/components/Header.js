79391342031eb640c971dc8e691053c931830163
Kasilucky <kasimca44@gmail.com>
2019-08-02 17:42:44 +0530

    header.js
    "change map function"

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,60 +28,59 @@ export default class Header extends Component {
       .catch(err => console.log("my err is ", err));
   }
   render() {
+    const userslist = this.state.users.map(user => (
+      <div className="card">
+        <div className="row no-gutters">
+          <div className="photo">
+            <img src={user.picture.large} className="image" alt="icon" />
+          </div>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>
+                  <i class="fa fa-user" />
+                  Firstname
+                </th>
+                <th>
+                  <i class="fa fa-user" />
+                  Lastname
+                </th>
+                <th>
+                  <i class="fa fa-envelope" />
+                  Email
+                </th>
+                <th>
+                  <i class="fa fa-heartbeat" aria-hidden="true" />
+                  Date of Birth
+                </th>
+                <th>
+                  <i class="fa fa-address-card-o" />
+                  Address
+                </th>
+                <th>
+                  <i class="fa fa-phone-square" aria-hidden="true" />
+                  Phone Number
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{user.name.first}</td>
+                <td>{user.name.last}</td>
+                <td>{user.email}</td>
+                <td>{user.dob.date}</td>
+                <td>{user.location.city}</td>
+                <td>{user.phone}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    ));
     return (
       <div className="main">
         <h1>RANDOM USER GENERATOR</h1>
-        <div>
-          {this.state.users.map(user => (
-            <div className="card">
-              <div className="row no-gutters">
-                <div className="photo">
-                  <img src={user.picture.large} className="image" alt="icon" />
-                </div>
-                <table class="table">
-                  <thead>
-                    <tr>
-                      <th>
-                        <i class="fa fa-user" />
-                        Firstname
-                      </th>
-                      <th>
-                        <i class="fa fa-user" />
-                        Lastname
-                      </th>
-                      <th>
-                        <i class="fa fa-envelope" />
-                        Email
-                      </th>
-                      <th>
-                        <i class="fa fa-heartbeat" aria-hidden="true" />
-                        Date of Birth
-                      </th>
-                      <th>
-                        <i class="fa fa-address-card-o" />
-                        Address
-                      </th>
-                      <th>
-                        <i class="fa fa-phone-square" aria-hidden="true" />
-                        Phone Number
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>{user.name.first}</td>
-                      <td>{user.name.last}</td>
-                      <td>{user.email}</td>
-                      <td>{user.dob.date}</td>
-                      <td>{user.location.city}</td>
-                      <td>{user.phone}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          ))}
-        </div>
+        <div>{userslist}</div>
       </div>
     );
   }
